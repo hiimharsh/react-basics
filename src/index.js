4cdfccf8e2bdb709d180681269cc89e04c8e3fef
Harsh Thakkar <harshthakkar46@gmail.com>
2017-05-04 23:44:31 +0530

    getting youtube videos list

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyCt3lYieC56jlyt4xLvo8-ode5nnkDaQyM'
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({
+      key: API_KEY,
+      term: 'surfboards'
+    }, (videos) => this.setState({ videos }) );
+  }
+
+  render () {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 
